fbda14555e283e06e84f38372c22d807c9006ad1
kudude703 <tku703@gmail.com>
2019-02-25 14:49:50 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,7 +17,7 @@ class minecraft {
     content => 'eula=true',
   }
   
-  file {'/etc/systemd/sysmtem/minecraft.service':
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
